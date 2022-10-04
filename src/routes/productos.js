e02bbc8cae9382d49e45697de21e3b03664d2423
Morimx <82421851+Morimx@users.noreply.github.com>
2022-10-03 22:08:14 -0300

    Añadiendo los middlewares

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,66 +1,17 @@
 const express = require("express");
 const { Router } = express;
-const Usuarioreg = require("../../registrarusuario");
-const usuario = new Usuarioreg("./data/usuarios.json");
 const router = Router();
-const Contenedor = require("../../constructor");
-const constructor = new Contenedor("./data/productos.json");
+const { getProducts, getProduct, addProduct, updateProduct, deleteProduct } = require("../controller/productosController");
+const { checkAdmin } = require("../controller/usuarioController");
 
-router.get("/", (req, res) => {
-  try {
-    res.send(constructor.getAll());
-  } catch (err) {
-    res.status(404).send(err);
-  }
-});
+router.get("/", getProducts);
 
-router.get("/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-    res.send(constructor.getById(parseInt(id)));
-  }
-  catch (err) {
-    res.status(404).send(err);
-  }
-});
+router.get("/:id", getProduct);
 
-router.post("/", (req, res) => {
-  try {
-    if (usuario.get(req.headers).administrador) {
-      const data = req.body;
-      constructor.save(data);
-      res.status(200).send("Producto agregado");
-    }
-  }
-  catch (err) {
-    res.status(401).send("No autorizado");
-  }
-});
+router.post("/", checkAdmin, addProduct);
 
-router.put("/:id", (req, res) => {
-  try {
-    if (usuario.get(req.headers).administrador) {
-      const { id } = req.params;
-      const prodNuevo = req.body;
-      const idInt = parseInt(id);
-      res.send(constructor.updateById(idInt, prodNuevo))
-    } else {
-      res.status(401).send("No autorizado");
-    }
-  } catch (err) {
-    res.status(401).send("No autorizado");
-  }
-});
+router.put("/:id", checkAdmin, updateProduct);
 
-router.delete("/:id", (req, res) => {
-  try {
-    if (usuario.get(req.headers).administrador) {
-      const { id } = req.params;
-      res.send(constructor.deleteById(parseInt(id)));
-    }
-  } catch (err) {
-    res.status(401).send("No autorizado");
-  }
-});
+router.delete("/:id", checkAdmin, deleteProduct);
 
 module.exports = router;
